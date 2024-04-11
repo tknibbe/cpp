df6113bd0b23367a4cbe5ebbe9098bf364b1e03b
Tymon Knibbe <tknibbe@f0r3s12.codam.nl>
2024-04-11 11:36:40 +0200

    cleaner output message

--- c01/ex05/Harl.cpp
+++ c01/ex05/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: tknibbe <tknibbe@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 20:29:19 by tknibbe           #+#    #+#             */
-/*   Updated: 2024/04/10 21:05:05 by tknibbe          ###   ########.fr       */
+/*   Updated: 2024/04/11 11:35:58 by tknibbe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,14 +34,14 @@ void	Harl::complain(std::string level)
 }
 
 void	Harl::debug(void){
-	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special ketchup burger. I really do!\n";
+	std::cout << "[DEBUG]\nI love having extra bacon for my 7XL-double-cheese-triple-pickle-special ketchup burger. I really do!\n";
 }
 void	Harl::info(void){
-	std::cout << "I cannot believe adding extra bacon costs more money. You didnt put enough bacon in my burger! If you did, I wouldnt be asking for more!\n";
+	std::cout << "[INFO]\nI cannot believe adding extra bacon costs more money. You didnt put enough bacon in my burger! If you did, I wouldnt be asking for more!\n";
 }
 void	Harl::warning(void){
-	std::cout << "I think I deserve to have some extra bacon for free. Ive been coming for years whereas you started working here since last month.\n";
+	std::cout << "[WARNING]\nI think I deserve to have some extra bacon for free. Ive been coming for years whereas you started working here since last month.\n";
 }
 void	Harl::error(void){
-	std::cout <<  "This is unacceptable! I want to speak to the manager now.\n";
+	std::cout <<  "[ERROR]\nThis is unacceptable! I want to speak to the manager now.\n";
 }
