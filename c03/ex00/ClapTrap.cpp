1cdaa5d425464b71af29b921d9194531a1117797
tymon knibbe <tymonknibbe@tymons-MacBook-Pro.local>
2024-04-30 13:23:39 +0200

    brak pushje

---
+++ c03/ex00/ClapTrap.cpp
@@ -0,0 +1,36 @@
+#include "ClapTrap.hpp"
+#include <iostream>
+
+ClapTrap::ClapTrap(std::string name) : _name(name){
+	std::cout << "constructor called\n";
+	_hitPoints = 10;
+	_energyPoints = 10;
+	_attackDamage = 10;
+}
+
+ClapTrap::~ClapTrap(){
+	std::cout << "destructor called\n";
+}
+
+ClapTrap::ClapTrap(ClapTrap& ref){
+	_name = ref._name;
+	//do i also  copy hp, ep and atkdmg?
+}
+
+ClapTrap&	ClapTrap::operator=(ClapTrap& ref){
+	if (this != &ref)
+		this->_name = ref._name;
+	//copy other data?
+	return *this;
+}
+
+void	ClapTrap::attack(const std::string& target){
+	if (_energyPoints == 0)
+		return ;
+	std::cout << "ClapTrap " << _name << " attacks " << target << ", causing " << _attackDamage << " points of damage!\n";
+	_energyPoints--;
+}
+
+void	ClapTrap::takeDamage(unsigned int amount){
+	
+}
--- c03/ex00/ClapTrap.hpp
+++ c03/ex00/ClapTrap.hpp
@@ -6,14 +6,16 @@
 class	ClapTrap{
 	private:
 		std::string _name;
-		int			_hit_points;
-		int			_energy_points;
-		int			_attack_damage;
+		int			_hitPoints;
+		int			_energyPoints;
+		int			_attackDamage;
 
 	public:
 		ClapTrap(std::string name);
 		~ClapTrap();
 		ClapTrap(ClapTrap& ref);
 		ClapTrap&	operator=(ClapTrap& ref);
-		
+		void	attack(const std::string& target);
+		void	takeDamage(unsigned int amount);
+		void	beRepaired(unsigned int amount);
 };
