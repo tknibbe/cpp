c938ad5840e25126a7014dd752d55c55659297ba
Tymon Knibbe <tknibbe@f1r2s13.codam.nl>
2024-07-10 14:43:27 +0200

    c07/ex01

--- c07/ex01/inc/iter.hpp
+++ c07/ex01/inc/iter.hpp
@@ -1,10 +1,32 @@
 #pragma once
+#include <iostream>
 
+template <typename T>
+using func = void (*) (T&);
 
-#define func void (*func) (T a)
-
-template <typename T> void iter(T& arr, int len, func){
+template <typename T>
+void iter(T* arr, size_t len, func<T> f){
 	for (size_t i=0;i<len;i++){
-		func(arr[i]);
+		f(arr[i]);
 	}
+}
+
+template <typename T>
+void print(T item){
+	std::cout << item << " ";
+}
+
+template <typename T>
+void	plusOne(T item){
+	++item;
+}
+
+template <typename T>
+void	printTestForMain(T* arr, int len){
+
+	std::cout << "before:\n";
+	::iter(arr, len, ::print);
+	::iter(arr, len, ::plusOne);
+	std::cout << "\nafter adding one:\n";
+	::iter(arr, len, ::print);
 }
--- c07/ex01/src/main.cpp
+++ c07/ex01/src/main.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
 #include "../inc/iter.hpp"
 
+
 int main( void ) {
 
-	//make arrays to test iter()
-	//make functions to give to iter()
 
+	int testInt[10];
+	for (int i=0;i<10;i++){
+		testInt[i] = 10 - i;
+	}
+	std::cout << "---int array---\n";
+	::printTestForMain(testInt, 10);
+
+	double testDouble[10] = {1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8,8, 9.9};
+	std::cout << "\n\n---Double arrar---\n";
+	::printTestForMain(testDouble, 10);
+
+	char testChar[5] = {'a', 'b', 'Z', ' ', '~'};
+	std::cout << "\n\n---Char arrar---\n";
+	::printTestForMain(testChar, 5);
+	std::cout << "\n";
+
+
+	//no nulltesting available for iter() since it doesnt compile
+	//trying to compare nullptr_t against a T* or function ptr
 	return 0;
 }
